501d56872098e910366dfa6527958f8caff21052
kouyoumikan <kouyoumikan136@gmail.com>
2022-01-06 12:10:28 +0900

    現在日時を表示するボタンの追加

--- js/main.js
+++ js/main.js
@@ -1,12 +1,24 @@
 const app = Vue.createApp({
+    el: "#app",
     data: () => ({
-        now: '-'
+        now: "00:00:00"
     }),
     methods: {
         addItem: function() {
             console.log('onClick!')
-            this.now = new Date().toLocaleString()
-        }
+            
+        },
+        time: function(e) { 
+            var date = new Date(); //インスタンスを生成
+
+            //日付と現在時刻のみ取得
+            this.now = new Date().toLocaleString();
+            
+            //現在時刻のみ取得
+            // this.now = date.getHours() + ":"
+            // + date.getMinutes() + ":" +
+            // date.getSeconds();
+          }
     }
-})
+});
 app.mount('#app')
