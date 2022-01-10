08a8ea542f8316b7e0a53f4714c786a46e2b7345
kouyoumikan <kouyoumikan136@gmail.com>
2022-01-10 16:08:55 +0900

    jsファイルの初期データの作成

---
+++ js/Vue_qiitaApi.js
@@ -0,0 +1,21 @@
+const app = Vue.createApp({
+    el: "#app",
+    data: () => ({ // データの初期値
+        
+
+    }),
+    // methods: { // メソッド
+        
+
+    // },
+
+    // computed: { // 算出プロパティ（getterとsetter）
+        
+    // },
+
+    // watch : { // 監視プロパティ（監視データ変更時はコンソールにnew値とold値を表示する処理）
+        
+
+    // },
+});
+app.mount('#app')
