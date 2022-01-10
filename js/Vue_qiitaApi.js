a73d6afa764577d356d49c251e4edaaac4fe66aa
kouyoumikan <kouyoumikan136@gmail.com>
2022-01-10 16:19:29 +0900

    初期データの作成

--- js/Vue_qiitaApi.js
+++ js/Vue_qiitaApi.js
@@ -1,21 +1,29 @@
 const app = Vue.createApp({
-    el: "#app",
+    
     data: () => ({ // データの初期値
-        
+        items: null, // APIから取得した検索結果のデータを格納
+        keyword: '', // ユーザーが検索した検索キーワードを格納
+        message: '', // ユーザーに表示するメッセージを格納
 
     }),
-    // methods: { // メソッド
+
+    mounted : function() {
+
+    },
+
+    methods: { // メソッド
         
 
-    // },
+    },
+
+    watch : { // 監視プロパティ（監視データ変更時はコンソールにnew値とold値を表示する処理）
+        
+
+    },
 
     // computed: { // 算出プロパティ（getterとsetter）
         
     // },
 
-    // watch : { // 監視プロパティ（監視データ変更時はコンソールにnew値とold値を表示する処理）
-        
-
-    // },
 });
 app.mount('#app')
