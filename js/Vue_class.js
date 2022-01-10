badd65d12cba2f62657f9146d658a7b8142aa05f
kouyoumikan <kouyoumikan136@gmail.com>
2022-01-10 17:24:44 +0900

    クラスのデータバインディングの基本

---
+++ js/Vue_class.js
@@ -0,0 +1,19 @@
+const app = Vue.createApp({
+    el: '#app',
+    data: () => ({ // データの初期値
+        
+
+    }),
+
+    watch : { // 監視プロパティ（監視データ変更時はコンソールにnew値とold値を表示する処理）
+        
+
+    },
+
+    methods: { // メソッド
+        
+
+    },
+
+});
+app.mount('#app')
